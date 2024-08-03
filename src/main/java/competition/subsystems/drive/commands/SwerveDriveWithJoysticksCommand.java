508b0df473a41e020dda7d5ea6c831938bf825c5
Rong <ronrzhu@gmail.com>
2024-08-03 13:15:19 -0700

    More robot scaling

--- src/main/java/competition/subsystems/drive/commands/SwerveDriveWithJoysticksCommand.java
+++ src/main/java/competition/subsystems/drive/commands/SwerveDriveWithJoysticksCommand.java
@@ -6,6 +6,8 @@ import competition.subsystems.pose.PoseSubsystem;
 import xbot.common.command.BaseCommand;
 import xbot.common.math.MathUtils;
 import xbot.common.math.XYPair;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
 
 import javax.inject.Inject;
 
@@ -15,12 +17,19 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
     DriveSubsystem drive;
     PoseSubsystem pose;
 
+    final DoubleProperty overallDrivingPowerScale;
+    final DoubleProperty overallTurningPowerScale;
+
     @Inject
-    public SwerveDriveWithJoysticksCommand(OperatorInterface oi, DriveSubsystem drive, PoseSubsystem pose) {
+    public SwerveDriveWithJoysticksCommand(OperatorInterface oi, DriveSubsystem drive, PoseSubsystem pose,
+                                           PropertyFactory pf) {
         this.drive = drive;
         this.pose = pose;
         this.oi = oi;
         this.addRequirements(drive);
+
+        this.overallDrivingPowerScale = pf.createPersistentProperty("DrivingPowerScale", 1.0);
+        this.overallTurningPowerScale = pf.createPersistentProperty("TurningPowerScale", 1.0);
     }
 
     @Override
@@ -30,21 +39,38 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
 
     @Override
     public void execute() {
-        // Get translate and rotation intents
-        XYPair translationIntent = getTranslationIntent();
-        double rotationIntent = getRotationIntent();
+        // Get raw human translate and rotation intents
+        XYPair translationIntent = getRawHumanTranslationIntent();
+        double rotationIntent = getRawHumanRotationIntent();
 
-        // Normalize and scale translationIntent
+        // Normalize and scale translationIntent, to prevent diagonal movement being faster
         if (translationIntent.getMagnitude() > 1) {
             double x = translationIntent.x;
             double y = translationIntent.y;
 
             // Normalize the intent
-            translationIntent.scale(1 / translationIntent.getMagnitude());
+            translationIntent = translationIntent.scale(1 / translationIntent.getMagnitude());
 
             // Scale the intent so that it reflects on how far the joystick is (assuming the values are -1 to 1)
             // (So that it will not always be the same speed as long as magnitude is > 1)
-            translationIntent.scale(Math.abs(x), Math.abs(y));
+            translationIntent = translationIntent.scale(Math.abs(x), Math.abs(y));
+        }
+
+        // Further scale translation and rotation intents if not driving full power
+        if (!drive.isUnlockFullDrivePowerActive()) {
+            // Scale translationIntent if precision modes active, values from XBot2024 repository
+            if (drive.isExtremePrecisionTranslationActive()) {
+                translationIntent = translationIntent.scale(0.15);
+            } else if (drive.isPrecisionTranslationActive()) {
+                translationIntent = translationIntent.scale(0.50);
+            }
+
+            if (drive.isPrecisionRotationActive()) {
+                rotationIntent *= 0.25;
+            }
+
+            translationIntent = translationIntent.scale(overallDrivingPowerScale.get());
+            rotationIntent *= overallTurningPowerScale.get();
         }
 
         // Field oriented drive will process the actual swerve movements for us
@@ -56,7 +82,7 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
         );
     }
 
-    private XYPair getTranslationIntent() {
+    private XYPair getRawHumanTranslationIntent() {
         double xIntent = MathUtils.deadband(oi.gamepad.getLeftVector().x, 0.15);
         double yIntent = MathUtils.deadband(oi.gamepad.getLeftVector().y, 0.15);
 
@@ -64,7 +90,7 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
         return new XYPair(xIntent, yIntent).rotate(-90);
     }
 
-    private double getRotationIntent() {
+    private double getRawHumanRotationIntent() {
         // Deadband is to prevent buggy joysticks/triggers
         double rotateLeftIntent = MathUtils.deadband(oi.gamepad.getLeftTrigger(), 0.15);
         double rotateRightIntent = MathUtils.deadband(oi.gamepad.getRightTrigger(), 0.15);

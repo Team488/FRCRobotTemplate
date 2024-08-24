7aca472377c14253cf808ecb134378a464ddcab5
Rong <ronrzhu@gmail.com>
2024-08-24 11:48:30 -0700

    Reorganized SwerveDriveWithJoysticksCommand
    Moved rotation suggestion logic into advisor.

--- src/main/java/competition/subsystems/drive/SwerveDriveRotationAdvisor.java
+++ src/main/java/competition/subsystems/drive/SwerveDriveRotationAdvisor.java
@@ -1,8 +1,18 @@
 package competition.subsystems.drive;
 
+import competition.operator_interface.OperatorInterface;
 import competition.subsystems.pose.PoseSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import xbot.common.logic.HumanVsMachineDecider;
 import xbot.common.logic.HumanVsMachineDecider.HumanVsMachineDeciderFactory;
+import xbot.common.math.ContiguousDouble;
+import xbot.common.math.XYPair;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
+
+import javax.inject.Inject;
 
 public class SwerveDriveRotationAdvisor {
     // Suggests rotation input for swerve drive
@@ -11,9 +21,114 @@ public class SwerveDriveRotationAdvisor {
     HumanVsMachineDecider hvmDecider;
     PoseSubsystem pose;
     DriveSubsystem drive;
+    OperatorInterface oi;
 
-    public SwerveDriveRotationAdvisor(PoseSubsystem pose, DriveSubsystem drive,
-                                      HumanVsMachineDeciderFactory hvmFactory) {
-        this.hvmDecider = hvmFactory;
+    DoubleProperty minimumMagnitudeToSnap;
+
+    @Inject
+    public SwerveDriveRotationAdvisor(PoseSubsystem pose, DriveSubsystem drive, PropertyFactory pf,
+                                      HumanVsMachineDeciderFactory hvmFactory, OperatorInterface oi) {
+        pf.setPrefix("SwerveDriveRotationAdvisor/");
+        this.hvmDecider = hvmFactory.create("SwerveDriveRotationAdvisor/");
+        this.drive = drive;
+        this.pose = pose;
+        this.oi = oi;
+
+        this.minimumMagnitudeToSnap = pf.createPersistentProperty("MinimumMagnitudeToSnap", 0.75);
+    }
+
+    public SuggestedRotationValue getSuggestedRotationValue(XYPair snappingInput, double triggerRotateIntent) {
+        SuggestedRotationValue suggested;
+        if (snappingInput.getMagnitude() >= minimumMagnitudeToSnap.get()) {
+            suggested = evaluateSnappingInput(snappingInput);
+        } else if (drive.getLookAtPointActive()) {
+            suggested = evaluateLookAtPoint();
+        } else if (drive.getStaticHeadingActive()) {
+            suggested = evaluateStaticHeading();
+        } else {
+            suggested = evaluateLastKnownHeading(triggerRotateIntent);
+        }
+
+        return suggested;
+    }
+
+    private SuggestedRotationValue evaluateSnappingInput(XYPair input) {
+        double heading = input.getAngle();
+
+        // Rebound the heading to be within -45 to 315 (diagnal X) then shift to 0 to 360 (for division purposes)
+        double reboundedHeading = ContiguousDouble.reboundValue(heading, -45, 315) + 45;
+
+        // Get which quadrant our rebounded heading is in
+        int quadrant = (int) (reboundedHeading / 90);
+        double desiredHeading = switch (quadrant) {
+            // Modify here if specific heading for certain quadrant(s)
+            default -> quadrant * 90;
+        };
+
+        if (DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Red) {
+            desiredHeading += 180;
+        }
+
+        // Shouldn't this be in more places? Hmm...
+        if (pose.getHeadingResetRecently()) {
+            drive.setDesiredHeading(pose.getCurrentHeading().getDegrees());
+        } else {
+            drive.setDesiredHeading(desiredHeading);
+        }
+        hvmDecider.reset();
+        return new SuggestedRotationValue(desiredHeading, SuggestedRotationValue.ValueType.DesiredHeading);
+    }
+
+    private SuggestedRotationValue evaluateLookAtPoint() {
+        // Don't think this takes in consideration of height...
+        Translation2d target = drive.getLookAtPointTarget();
+
+        Pose2d currentPose = pose.getCurrentPose2d();
+        Translation2d currentXY = new Translation2d(currentPose.getX(), currentPose.getY());
+
+        double desiredHeading = currentXY.minus(target).getAngle().getDegrees() + 180;
+        drive.setDesiredHeading(desiredHeading); // Does this need a recently heading reset check?
+        return new SuggestedRotationValue(desiredHeading, SuggestedRotationValue.ValueType.DesiredHeading);
+    }
+
+    private SuggestedRotationValue evaluateStaticHeading() {
+        double desiredHeading = drive.getStaticHeadingTarget().getDegrees();
+        drive.setDesiredHeading(desiredHeading);
+        return new SuggestedRotationValue(desiredHeading, SuggestedRotationValue.ValueType.DesiredHeading);
+    }
+
+    private SuggestedRotationValue evaluateLastKnownHeading(double triggerRotateIntent) {
+        HumanVsMachineDecider.HumanVsMachineMode recommendedMode = hvmDecider.getRecommendedMode(triggerRotateIntent);
+
+        if (pose.getHeadingResetRecently()) {
+            drive.setDesiredHeading(pose.getCurrentHeading().getDegrees());
+        }
+
+        return switch (recommendedMode) {
+            case HumanControl -> new SuggestedRotationValue(
+                    scaleHumanRotationIntent(triggerRotateIntent),
+                    SuggestedRotationValue.ValueType.HeadingPower
+            );
+            case InitializeMachineControl -> {
+                drive.setDesiredHeading(pose.getCurrentHeading().getDegrees());
+                yield new SuggestedRotationValue();
+            }
+            case MachineControl -> new SuggestedRotationValue(
+                    drive.getDesiredHeading(),
+                    SuggestedRotationValue.ValueType.DesiredHeading
+            );
+            case Coast -> new SuggestedRotationValue();
+        };
+    }
+
+    private double scaleHumanRotationIntent(double intent) {
+        if (drive.isPrecisionRotationActive()) {
+            intent *= 0.25;
+        }
+        return intent;
+    }
+
+    public void resetDecider() {
+        hvmDecider.reset();
     }
 }
--- src/main/java/competition/subsystems/drive/commands/SwerveDriveWithJoysticksCommand.java
+++ src/main/java/competition/subsystems/drive/commands/SwerveDriveWithJoysticksCommand.java
@@ -3,14 +3,9 @@ package competition.subsystems.drive.commands;
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.SuggestedRotationValue;
+import competition.subsystems.drive.SwerveDriveRotationAdvisor;
 import competition.subsystems.pose.PoseSubsystem;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import xbot.common.command.BaseCommand;
-import xbot.common.logic.HumanVsMachineDecider;
-import xbot.common.logic.HumanVsMachineDecider.HumanVsMachineDeciderFactory;
-import xbot.common.math.ContiguousDouble;
 import xbot.common.math.MathUtils;
 import xbot.common.math.XYPair;
 import xbot.common.properties.DoubleProperty;
@@ -25,34 +20,33 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
     OperatorInterface oi;
     DriveSubsystem drive;
     PoseSubsystem pose;
-    HumanVsMachineDecider hvmDecider;
     HeadingModule headingModule;
 
     DoubleProperty overallDrivingPowerScale;
     DoubleProperty overallTurningPowerScale;
-    final DoubleProperty minimumMagnitudeToSnap;
+
+    SwerveDriveRotationAdvisor advisor;
 
     @Inject
     public SwerveDriveWithJoysticksCommand(
             OperatorInterface oi, DriveSubsystem drive, PoseSubsystem pose, PropertyFactory pf,
-            HeadingModuleFactory headingModuleFactory, HumanVsMachineDeciderFactory hvmFactory) {
+            HeadingModuleFactory headingModuleFactory, SwerveDriveRotationAdvisor advisor) {
         pf.setPrefix(this);
         this.drive = drive;
         this.pose = pose;
         this.oi = oi;
-        this.hvmDecider = hvmFactory.create(this.getPrefix());
         this.headingModule = headingModuleFactory.create(drive.getRotateToHeadingPid());
+        this.advisor = advisor;
 
         this.overallDrivingPowerScale = pf.createPersistentProperty("DrivingPowerScale", 1.0);
         this.overallTurningPowerScale = pf.createPersistentProperty("TurningPowerScale", 1.0);
-        this.minimumMagnitudeToSnap = pf.createPersistentProperty("MinimumMagnitudeToSnap", 0.75);
         this.addRequirements(drive);
     }
 
     @Override
     public void initialize() {
         log.info("Initializing");
-        hvmDecider.reset();
+        advisor.resetDecider();
         drive.setDesiredHeading(pose.getCurrentHeading().getDegrees());
     }
 
@@ -67,7 +61,6 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
 
         // Checks snapping to side or other rotation features to get suggested intent
         rotationIntent = getSuggestedRotationIntent(rotationIntent);
-
 
         // Further scale translation and rotation intents if not driving full power
         if (!drive.isUnlockFullDrivePowerActive()) {
@@ -102,7 +95,7 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
         return rotateLeftIntent - rotateRightIntent;
     }
 
-    private double getSuggestedRotationIntent(double triggerRotateIntent) {
+    public double getSuggestedRotationIntent(double triggerRotateIntent) {
         // Firstly, I would like to apologize for the namings of these functions...
 
         // Checks the right joystick input to see if we want to snap to a certain side
@@ -110,87 +103,8 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
         // And of course, we must rotate -90 (similar to how we got raw translation) for default alignment
         XYPair joystickInput = new XYPair(-oi.gamepad.getRightVector().x, oi.gamepad.getRightVector().y).rotate(-90);
 
-        SuggestedRotationValue suggested;
-        if (joystickInput.getMagnitude() >= minimumMagnitudeToSnap.get()) {
-            suggested = evaluateSnappingInput(joystickInput);
-        } else if (drive.getLookAtPointActive()) {
-            suggested = evaluateLookAtPoint();
-        } else if (drive.getStaticHeadingActive()) {
-            suggested = evaluateStaticHeading();
-        } else {
-            suggested = evaluateLastKnownHeading(triggerRotateIntent);
-        }
-
+        SuggestedRotationValue suggested = advisor.getSuggestedRotationValue(joystickInput, triggerRotateIntent);
         return processSuggestedRotationValueIntoPower(suggested);
-    }
-
-    private SuggestedRotationValue evaluateSnappingInput(XYPair input) {
-        double heading = input.getAngle();
-
-        // Rebound the heading to be within -45 to 315 (diagnal X) then shift to 0 to 360 (for division purposes)
-        double reboundedHeading = ContiguousDouble.reboundValue(heading, -45, 315) + 45;
-
-        // Get which quadrant our rebounded heading is in
-        int quadrant = (int) (reboundedHeading / 90);
-        double desiredHeading = switch (quadrant) {
-            // Modify here if specific heading for certain quadrant(s)
-            default -> quadrant * 90;
-        };
-
-        if (DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Red) {
-            desiredHeading += 180;
-        }
-
-        // Shouldn't this be in more places? Hmm...
-        if (pose.getHeadingResetRecently()) {
-            drive.setDesiredHeading(pose.getCurrentHeading().getDegrees());
-        } else {
-            drive.setDesiredHeading(desiredHeading);
-        }
-        hvmDecider.reset();
-        return new SuggestedRotationValue(desiredHeading, SuggestedRotationValue.ValueType.DesiredHeading);
-    }
-
-    private SuggestedRotationValue evaluateLookAtPoint() {
-        // Don't think this takes in consideration of height...
-        Translation2d target = drive.getLookAtPointTarget();
-
-        Pose2d currentPose = pose.getCurrentPose2d();
-        Translation2d currentXY = new Translation2d(currentPose.getX(), currentPose.getY());
-
-        double desiredHeading = currentXY.minus(target).getAngle().getDegrees() + 180;
-        drive.setDesiredHeading(desiredHeading); // Does this need a recently heading reset check?
-        return new SuggestedRotationValue(desiredHeading, SuggestedRotationValue.ValueType.DesiredHeading);
-    }
-
-    private SuggestedRotationValue evaluateStaticHeading() {
-        double desiredHeading = drive.getStaticHeadingTarget().getDegrees();
-        drive.setDesiredHeading(desiredHeading);
-        return new SuggestedRotationValue(desiredHeading, SuggestedRotationValue.ValueType.DesiredHeading);
-    }
-
-    private SuggestedRotationValue evaluateLastKnownHeading(double triggerRotateIntent) {
-        HumanVsMachineDecider.HumanVsMachineMode recommendedMode = hvmDecider.getRecommendedMode(triggerRotateIntent);
-
-        if (pose.getHeadingResetRecently()) {
-            drive.setDesiredHeading(pose.getCurrentHeading().getDegrees());
-        }
-
-        return switch (recommendedMode) {
-            case HumanControl -> new SuggestedRotationValue(
-                    scaleHumanRotationIntent(triggerRotateIntent),
-                    SuggestedRotationValue.ValueType.HeadingPower
-            );
-            case InitializeMachineControl -> {
-                drive.setDesiredHeading(pose.getCurrentHeading().getDegrees());
-                yield new SuggestedRotationValue();
-            }
-            case MachineControl -> new SuggestedRotationValue(
-                    drive.getDesiredHeading(),
-                    SuggestedRotationValue.ValueType.DesiredHeading
-            );
-            case Coast -> new SuggestedRotationValue();
-        };
     }
 
     private XYPair processTranslationIntent(XYPair intent) {
@@ -216,13 +130,6 @@ public class SwerveDriveWithJoysticksCommand extends BaseCommand {
             } else if (drive.isPrecisionTranslationActive()) {
                 intent = intent.scale(0.50);
             }
-        }
-        return intent;
-    }
-
-    private double scaleHumanRotationIntent(double intent) {
-        if (drive.isPrecisionRotationActive()) {
-            intent *= 0.25;
         }
         return intent;
     }

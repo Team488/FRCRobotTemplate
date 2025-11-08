8fbc377be1fe18fd170e10f10eeb801b8bd9f045
Stephen Just <stephenjust@gmail.com>
2025-11-08 13:36:09 -0800

    Swerve offsets to use WPI Units (#72)

--- src/main/java/competition/electrical_contract/ElectricalContract.java
+++ src/main/java/competition/electrical_contract/ElectricalContract.java
@@ -1,10 +1,10 @@
 package competition.electrical_contract;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import xbot.common.injection.electrical_contract.CANMotorControllerInfo;
 import xbot.common.injection.electrical_contract.DeviceInfo;
 import xbot.common.injection.electrical_contract.XSwerveDriveElectricalContract;
 import xbot.common.injection.swerve.SwerveInstance;
-import xbot.common.math.XYPair;
 
 public abstract class ElectricalContract implements XSwerveDriveElectricalContract {
     public abstract boolean isDriveReady();
@@ -17,5 +17,5 @@ public abstract class ElectricalContract implements XSwerveDriveElectricalContra
 
     public abstract DeviceInfo getSteeringEncoder(SwerveInstance swerveInstance);
 
-    public abstract XYPair getSwerveModuleOffsetsInInches(SwerveInstance swerveInstance);
+    public abstract Translation2d getSwerveModuleOffsets(SwerveInstance swerveInstance);
 }

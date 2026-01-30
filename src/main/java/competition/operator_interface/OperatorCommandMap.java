aeac94c9006cc538f5b740cde95adf90fa185487
WeWaWu <wwu5528@gmail.com>
2026-01-29 20:14:36 -0800

    Added DebugSwerveDriveCommand

--- src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -4,6 +4,9 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import xbot.common.subsystems.pose.commands.SetRobotHeadingCommand;
+import competition.subsystems.drive.commands.DebugSwerveModuleCommand;
+import competition.subsystems.drive.commands.SwerveDriveWithJoysticksCommand;
+import xbot.common.subsystems.drive.swerve.commands.ChangeActiveSwerveModuleCommand;
 
 /**
  * Maps operator interface buttons to commands
@@ -21,5 +24,19 @@ public class OperatorCommandMap {
             SetRobotHeadingCommand resetHeading) {
         resetHeading.setHeadingToApply(0);
         operatorInterface.gamepad.getifAvailable(1).onTrue(resetHeading);
+    }
+
+    @Inject
+    public void setupDriveCommands(OperatorInterface operatorInterface,
+                                   SetRobotHeadingCommand resetHeading,
+                                   DebugSwerveModuleCommand debugModule,
+                                   ChangeActiveSwerveModuleCommand changeActiveModule,
+                                   SwerveDriveWithJoysticksCommand typicalSwerveDrive) {
+        resetHeading.setHeadingToApply(0);
+        operatorInterface.driverGamepad.getifAvailable(1).onTrue(resetHeading);
+
+        operatorInterface.driverGamepad.getPovIfAvailable(0).onTrue(debugModule);
+        operatorInterface.driverGamepad.getPovIfAvailable(90).onTrue(changeActiveModule);
+        operatorInterface.driverGamepad.getPovIfAvailable(180).onTrue(typicalSwerveDrive);
     }
 }

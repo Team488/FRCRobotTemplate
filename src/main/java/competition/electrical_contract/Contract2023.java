a92b2f8e04071a301b9f6c28308df2e96391db5f
Rongrui Zhu <ronrzhu@gmail.com>
2026-05-28 20:18:34 -0700

    feat: migrate new 2026 contract system to template

--- src/main/java/competition/electrical_contract/Contract2023.java
+++ src/main/java/competition/electrical_contract/Contract2023.java
@@ -1,28 +1,37 @@
 package competition.electrical_contract;
 
-import javax.inject.Inject;
-
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-
 import competition.subsystems.pose.PoseSubsystem;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Distance;
+import xbot.common.controls.sensors.XGyro;
 import xbot.common.injection.electrical_contract.CANBusId;
 import xbot.common.injection.electrical_contract.CANMotorControllerInfo;
-import xbot.common.injection.electrical_contract.CANMotorControllerOutputConfig;
+import xbot.common.injection.electrical_contract.CameraInfo;
 import xbot.common.injection.electrical_contract.DeviceInfo;
+import xbot.common.injection.electrical_contract.IMUInfo;
 import xbot.common.injection.electrical_contract.MotorControllerType;
+import xbot.common.injection.electrical_contract.PowerSource;
+import xbot.common.injection.electrical_contract.SparkMaxMotorControllerOutputConfig;
 import xbot.common.injection.swerve.SwerveInstance;
-import xbot.common.math.XYPair;
+import xbot.common.subsystems.vision.CameraCapabilities;
 
+import javax.inject.Inject;
+import java.util.EnumSet;
+import java.util.Set;
+
+import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Inches;
 
-public class CompetitionContract extends ElectricalContract {
-
-    protected final double simulationScalingValue = 256.0 * PoseSubsystem.INCHES_IN_A_METER;
+public class Contract2023 extends GeneralContract {
 
     @Inject
-    public CompetitionContract() {}
+    public Contract2023() {
+        super(Set.of());
+    }
 
     @Override
     public boolean isDriveReady() {
@@ -34,18 +43,6 @@ public class CompetitionContract extends ElectricalContract {
         return true;
     }
 
-    protected String getDriveControllerName(SwerveInstance swerveInstance) {
-        return "DriveSubsystem/" + swerveInstance.label() + "/Drive";
-    }
-
-    protected String getSteeringControllerName(SwerveInstance swerveInstance) {
-        return "DriveSubsystem/" + swerveInstance.label() + "/Steering";
-    }
-
-    protected String getSteeringEncoderControllerName(SwerveInstance swerveInstance) {
-        return "DriveSubsystem/" + swerveInstance.label() + "/SteeringEncoder";
-    }
-
     @Override
     public CANMotorControllerInfo getDriveMotor(SwerveInstance swerveInstance) {
         return switch (swerveInstance.label()) {
@@ -55,36 +52,38 @@ public class CompetitionContract extends ElectricalContract {
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             31,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(60)));
             case "FrontRightDrive" ->
                     new CANMotorControllerInfo(
                             getDriveControllerName(swerveInstance),
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             29,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(60)));
             case "RearLeftDrive" ->
                     new CANMotorControllerInfo(
                             getDriveControllerName(swerveInstance),
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             38,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(60)));
             case "RearRightDrive" ->
                     new CANMotorControllerInfo(
                             getDriveControllerName(swerveInstance),
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             21,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(60)));
             default -> null;
         };
     }
 
     @Override
     public CANMotorControllerInfo getSteeringMotor(SwerveInstance swerveInstance) {
-        double simulationScalingValue = 1.0;
-
         return switch (swerveInstance.label()) {
             case "FrontLeftDrive" ->
                     new CANMotorControllerInfo(
@@ -92,28 +91,32 @@ public class CompetitionContract extends ElectricalContract {
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             30,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(40)));
             case "FrontRightDrive" ->
                     new CANMotorControllerInfo(
                             getDriveControllerName(swerveInstance),
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             28,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(40)));
             case "RearLeftDrive" ->
                     new CANMotorControllerInfo(
                             getDriveControllerName(swerveInstance),
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             39,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(40)));
             case "RearRightDrive" ->
                     new CANMotorControllerInfo(
                             getDriveControllerName(swerveInstance),
                             MotorControllerType.SparkMax,
                             CANBusId.RIO,
                             20,
-                            new CANMotorControllerOutputConfig());
+                            new SparkMaxMotorControllerOutputConfig()
+                                    .withSmartCurrentLimit(Amps.of(40)));
             default -> null;
         };
     }
@@ -152,18 +155,62 @@ public class CompetitionContract extends ElectricalContract {
         return 12.1; // Documented value for WCP x2i.
     }
 
-    /**
-     * Returns the largest radius of the robot.
-     *
-     * @return The longest point to the edge of the robot back to the center.
-     */
-    @Override
-    public Distance getRadiusOfRobot() {
-        return null;
-    }
-
     @Override
     public double getDriveGearRatio() {
         return 6.48; // Documented value for WCP x2i with X3 10t gears.
     }
+
+    @Override
+    public CameraInfo[] getCameraInfo() {
+        double sideAprilCameraXDisplacement = 6.75 / PoseSubsystem.INCHES_IN_A_METER;
+        double sideAprilCameraYDisplacement = 7.75 / PoseSubsystem.INCHES_IN_A_METER;
+        double sideAprilCameraZDisplacement = 11 / PoseSubsystem.INCHES_IN_A_METER;
+        double sideAprilCameraPitch = Math.toRadians(-25);
+
+        return new CameraInfo[]{
+                new CameraInfo("Apriltag_Left_Camera",
+                        "AprilTagLeft",
+                        new Transform3d(new Translation3d(
+                                sideAprilCameraXDisplacement,
+                                sideAprilCameraYDisplacement,
+                                sideAprilCameraZDisplacement),
+                                new Rotation3d(0, sideAprilCameraPitch, Math.toRadians(90))),
+                        EnumSet.of(CameraCapabilities.APRIL_TAG)),
+                new CameraInfo("Apriltag_Right_Camera",
+                        "AprilTagRight",
+                        new Transform3d(new Translation3d(
+                                -sideAprilCameraXDisplacement,
+                                sideAprilCameraYDisplacement,
+                                sideAprilCameraZDisplacement),
+                                new Rotation3d(0, sideAprilCameraPitch, Math.toRadians(270))),
+                        EnumSet.of(CameraCapabilities.APRIL_TAG)),
+                new CameraInfo("Apriltag_Front_Camera",
+                        "AprilTagFront",
+                        new Transform3d(new Translation3d(
+                                0.0,
+                                13.125 / PoseSubsystem.INCHES_IN_A_METER,
+                                14.0 / PoseSubsystem.INCHES_IN_A_METER),
+                                new Rotation3d(0, Math.toRadians(-30), Math.toRadians(0))),
+                        EnumSet.of(CameraCapabilities.APRIL_TAG)),
+                new CameraInfo("Apriltag_Back_Camera",
+                        "AprilTagBack",
+                        new Transform3d(new Translation3d(
+                                0,
+                                -12.959212 / PoseSubsystem.INCHES_IN_A_METER,
+                                17.768664 / PoseSubsystem.INCHES_IN_A_METER),
+                                new Rotation3d(0, Math.toRadians(-15), Math.toRadians(180))),
+                        EnumSet.of(CameraCapabilities.APRIL_TAG)),
+        };
+    }
+
+    @Override
+    public IMUInfo getIMUInfo() {
+        return new IMUInfo(XGyro.InterfaceType.spi, PowerSource.RIO);
+    }
+
+    @Override
+    public Distance getRadiusOfRobot() {
+        return Units.Inches.of(18);
+    }
+
 }
--- src/main/java/competition/electrical_contract/ElectricalContract.java
+++ src/main/java/competition/electrical_contract/ElectricalContract.java
@@ -1,21 +1,71 @@
 package competition.electrical_contract;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Distance;
 import xbot.common.injection.electrical_contract.CANMotorControllerInfo;
 import xbot.common.injection.electrical_contract.DeviceInfo;
+import xbot.common.injection.electrical_contract.IMUInfo;
+import xbot.common.injection.electrical_contract.PDHPort;
+import xbot.common.injection.electrical_contract.XCameraElectricalContract;
 import xbot.common.injection.electrical_contract.XSwerveDriveElectricalContract;
 import xbot.common.injection.swerve.SwerveInstance;
 
-public abstract class ElectricalContract implements XSwerveDriveElectricalContract {
-    public abstract boolean isDriveReady();
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+
+public abstract class ElectricalContract implements XSwerveDriveElectricalContract, XCameraElectricalContract {
+    private final Set<Hardware> readinessSet;
+
+    protected ElectricalContract(Set<Hardware> readinessSet) {
+        this.readinessSet = readinessSet;
+    }
+
+    public boolean isReady(Hardware hardware) {
+        return readinessSet.contains(hardware);
+    }
+
+    // TODO: Remove these later
+    public abstract boolean isDriveReady();
     public abstract boolean areCanCodersReady();
 
     public abstract CANMotorControllerInfo getDriveMotor(SwerveInstance swerveInstance);
-
     public abstract CANMotorControllerInfo getSteeringMotor(SwerveInstance swerveInstance);
-
     public abstract DeviceInfo getSteeringEncoder(SwerveInstance swerveInstance);
-
     public abstract Translation2d getSwerveModuleOffsets(SwerveInstance swerveInstance);
+    public abstract IMUInfo getIMUInfo();
+
+    /**
+     * Returns additional PDH connections for non-motor devices (e.g., VRMs, PCMs, buck converters, etc.)
+     * Override this method in specific contract implementations to specify these connections.
+     * Multiple non-motor devices may share a PDH port (e.g., two buck converters on one port).
+     */
+    public Map<PDHPort, List<String>> getAdditionalPDHConnections() {
+        return new HashMap<>();
+    }
+
+    /**
+     * Returns power branch connections for intermediate converters (e.g., buck converters, VRMs).
+     * Maps converter name to the list of devices it powers.
+     * Override this method in specific contract implementations to document the power chain.
+     */
+    public Map<String, List<String>> getAdditionalPowerBranches() {
+        return new HashMap<>();
+    }
+
+    public abstract Distance getRadiusOfRobot();
+
+    protected String getDriveControllerName(SwerveInstance swerveInstance) {
+        return "DriveSubsystem/" + swerveInstance.label() + "/Drive";
+    }
+
+    protected String getSteeringControllerName(SwerveInstance swerveInstance) {
+        return "DriveSubsystem/" + swerveInstance.label() + "/Steering";
+    }
+
+    protected String getSteeringEncoderControllerName(SwerveInstance swerveInstance) {
+        return "DriveSubsystem/" + swerveInstance.label() + "/SteeringEncoder";
+    }
 }

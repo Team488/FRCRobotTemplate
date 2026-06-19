93ea5ab38725e97db02559c83a4bc6325ee9fcc9
Rongrui Zhu <ronrzhu@gmail.com>
2026-06-18 19:54:37 -0700

    fix: add general contract

---
+++ src/main/java/competition/electrical_contract/GeneralContract.java
@@ -0,0 +1,10 @@
+package competition.electrical_contract;
+
+import java.util.Set;
+
+public abstract class GeneralContract extends ElectricalContract {
+
+    protected GeneralContract(Set<Hardware> readinessSet) {
+        super(readinessSet);
+    }
+}
